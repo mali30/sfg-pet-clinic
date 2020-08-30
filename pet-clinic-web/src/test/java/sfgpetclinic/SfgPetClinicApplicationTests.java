f924beedb327547f36b79676cd3abba37aafe4e6
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-08-30 15:37:31 -0400

    refactored to mutlimodule maven project. Closes #1.

---
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = com.mohamedali.sfgpetclinic.SfgPetClinicApplication.class)
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
