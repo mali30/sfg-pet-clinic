905d01bd48042b03b559902ad512f69d4989e697
Mohamed Ali <mohamed.ali@cfacorp.com>
2020-09-26 12:44:36 -0400

    added vet list and updated controller. closes #14

--- pet-clinic-web/src/main/java/com/mohamedali/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/mohamedali/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package com.mohamedali.sfgpetclinic.controllers;
 
+import com.mohamedali.sfgpetclinic.services.VetService;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("/vets")
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets list" , vetService.findAll());
         return "vets page";
     }
 }
